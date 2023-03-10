d686b5c7da9bb39dffc0c09435c6b8dfc2b70c56
MollyZen <denis.saltikov@gmail.com>
2023-03-11 00:05:29 +0300

    temporary solution to disable redis access on startup

--- src/main/java/ru/saltykov/diploma/DiplomaApplication.java
+++ src/main/java/ru/saltykov/diploma/DiplomaApplication.java
@@ -2,8 +2,9 @@ package ru.saltykov.diploma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisAutoConfiguration.class)
 public class DiplomaApplication {
 
     public static void main(String[] args) {
