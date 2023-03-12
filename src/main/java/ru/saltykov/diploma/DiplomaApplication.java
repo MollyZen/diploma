e5cad0147829d3f79862b379087c8981d1011476
MollyZen <denis.saltikov@gmail.com>
2023-03-12 16:32:25 +0300

    enabled assembly into zip with static content, disable mongodb autoconfig

--- src/main/java/ru/saltykov/diploma/DiplomaApplication.java
+++ src/main/java/ru/saltykov/diploma/DiplomaApplication.java
@@ -3,8 +3,11 @@ package ru.saltykov.diploma;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication(exclude = RedisAutoConfiguration.class)
+@EnableWebMvc
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class, MongoAutoConfiguration.class})
 public class DiplomaApplication {
 
     public static void main(String[] args) {
